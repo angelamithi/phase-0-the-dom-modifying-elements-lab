fc3a3e227bbb0827dac11f5e62a166ade8bd06d2
+ <+>
2023-09-13 17:56:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainID=document.querySelector('main#main');
+mainID.remove();
+let myName="Angela";
+const newHeader=document.createElement('h1');
+newHeader.id="victory";
+newHeader.innerHTML=`${myName} is the champion`;
